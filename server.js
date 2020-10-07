0af58b25f2b1b81251f4caaca124bcbb4d0e7ee0
Florian Allione <florian.allione@ynov.com>
2020-10-07 17:33:34 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('hi');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, () => { console.log(`Listening at http://localhost:${port}`); });
